0d9a72067a74ea0117bdb3bb881f7781e212f67d
Luana Brito <lccb@cin.ufpe.br>
2023-05-04 22:11:34 -0300

    adições

--- interface.py
+++ interface.py
@@ -1,16 +1,43 @@
 import tkinter as tk
+from tkinter import messagebox, filedialog
 import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
 import numpy as np
+import pandas as pd
 import projeto_cont as dados_tabela
-    
+import pathlib
+
+from janelinha import Janela
+
+
 class Interfazinha:
     def __init__(self):
-        self.criar_janela()
+        self.tabela = None
+        self.contas_selecionadas = set(dados_tabela.contas_interesse)
+
+    def abrir_janela_especial(self):
+        if type(self.tabela) != type(None):
+            janela = Janela(self.app, self.contas,
+                            self.contas_selecionadas, self.update_tabela)
+            janela.grab_set()
+        else:
+            messagebox.showerror(
+                "Sem planilha", "Você precisa carregar uma planilha.")
+
+    def carregar_tabela(self, tabela):
+        self.tabela = tabela
+        self.cb_input_uf['values'] = dados_tabela.get_all_uf(self.tabela)
+        self.contas = sorted(list(self.tabela["Conta"].unique()))
+
+    def update_tabela(self):
+        print("Update tabela")
+        self.processar()
 
     def criar_janela(self):
         self.app = ttk.Window(themename='cyborg')
-        self.app.geometry('900x1200')
+        screen_width = self.app.winfo_screenwidth()
+        screen_height = self.app.winfo_screenheight()
+        self.app.geometry("{}x{}".format(screen_width, screen_height))
 
         self.label = ttk.Label(self.app, text='Projeto Contabilidade')
         self.label.pack(pady=30)
@@ -18,69 +45,118 @@ class Interfazinha:
 
         # RadioButtons (Estados ou Municípios)
 
-        self.var = tk.StringVar()
+        self.option_selected = tk.StringVar()
         self.header = ttk.Frame(self.app)
         self.header.pack(side='top', fill='y')
 
-        self.option1 = ttk.Radiobutton(
-            self.header, text="Estados", variable=self.var, value="Estados", command=self.event_RadioButton)
-        self.option1.pack(side=tk.LEFT, padx=20, pady=10)
-        self.option2 = ttk.Radiobutton(
-            self.header, text="Municípios", variable=self.var, value="Municípios", command=self.event_RadioButton)
-        self.option2.pack(side=tk.LEFT, padx=20, pady=10)
+        self.radio_option_estado = ttk.Radiobutton(
+            self.header, text="Estados", variable=self.option_selected, value="Estados", command=self.event_RadioButton)  # Chama essa callback quando o botao radio é mudado(selecionado)
+        self.radio_option_estado.pack(side=tk.LEFT, padx=10, pady=10)
 
-        self.cb_input_principal = ttk.Combobox(self.header, width=50)
-        self.cb_input_principal.set('Selecione a opção')
-        self.cb_input_principal.pack(pady=10, padx=10, side='left')
+        self.radio_option_municipio = ttk.Radiobutton(
+            self.header, text="Municípios", variable=self.option_selected, value="Municípios", command=self.event_RadioButton)  # Chama essa callback quando o botao radio é mudado(selecionado)
+        self.radio_option_municipio.pack(side=tk.LEFT, padx=10, pady=10)
+
+        # Opção Estados
+        self.cb_input_uf = ttk.Combobox(self.header, width=10)
+        self.cb_input_uf.set('UF')
+        self.cb_input_uf.pack(pady=10, padx=10, side='left')
+
+        # Chama essa callback quando um elemento é mudado no ComboBox
+        self.cb_input_uf.bind("<<ComboboxSelected>>", self.TextBoxUpdate)
+
+        # Opção Município
+        self.cb_input_municipio = ttk.Combobox(self.header, width=50)
+        self.cb_input_municipio.set('Selecionar Município')
+        self.cb_input_municipio.pack(pady=10, padx=10, side='left')
+
+        self.cb_input_municipio.configure(state="disabled")
+        self.cb_input_uf.configure(state="disabled")
 
         self.style = ttk.Style()
         self.style.configure('Treeview',
-            background = '#371c4b',
-            foreground = 'white',
-            rowheight=25,
-            
-        )
-        self.style.map('Treeview',
-            background=[('selected', '#232323')]
-        )
+                             background='#371c4b',
+                             foreground='white',
+                             rowheight=35,
 
-        button_frame = ttk.Frame(self.app)
-        button_frame.pack(side='top', pady=10)
-        self.treeview = ttk.Treeview( button_frame, columns=['Tipo da Conta']+['Despesas Empenhadas', 'Despesas Liquidadas', 'Despesas Pagas', 'Seguridade Social'])
+                             )
+        self.style.map('Treeview',
+                       background=[('selected', '#232323')]
+                       )
+
+        self.tabela_frame = ttk.Frame(self.app)
+        self.tabela_frame.pack(side='top', pady=10)
+
+        self.treeview = ttk.Treeview(self.tabela_frame, columns=['Tipo da Conta']+ dados_tabela.coluna_interesse)
+        self.treeview['show'] = 'headings'
+        
+        self.treeview.column('Tipo da Conta', width=315)
+        self.treeview.heading('Tipo da Conta', text='Tipo da Conta')
+
+        # Outras colunas
+        for column in self.treeview['columns'][1:]:
+            self.treeview.column(column, width=200, anchor='center')
+            self.treeview.heading(column, text=column)
 
         processar_btn = ttk.Button(self.header,
                                    text='Processar',
                                    bootstyle='SUCCESS-OUTLINE',
-                                   command=lambda: self.search(
-                                       tabela, self.cb_input_principal.get())
+                                   command=self.processar
                                    )
         processar_btn.pack(pady=10, padx=10, side='left')
 
-    def event_RadioButton(self):
-        if self.var.get() == "Estados":
-            todos_uf = dados_tabela.get_all_uf(tabela)
-            self.cb_input_principal['values'] = todos_uf
-            self.cb_input_principal.set('Selecione o Estado')
-        else:
-            todos_municipios = dados_tabela.get_all_municipios(
-                tabela)  
-            self.cb_input_principal['values'] = todos_municipios
-            self.cb_input_principal.set('Selecione o Município')
+        self.carregar_btn = ttk.Button(self.header,
+                                       text='Carregar',
+                                       bootstyle='SUCCESS-OUTLINE',
+                                       command=self.carregar_planilha
+                                       )
+        self.carregar_btn.pack(pady=10, padx=10, side='left')
 
-    def search(self, tabela, cb_input_principal: str):
-        if len(cb_input_principal) == 2:
-            resultados =  dados_tabela.get_uf(tabela, cb_input_principal, dados_tabela.contas_interesse, dados_tabela.coluna_interesse)
-        elif len(cb_input_principal) > 2: 
-            resultados = dados_tabela.get_cidade(tabela, cb_input_principal, dados_tabela.contas_interesse, dados_tabela.coluna_interesse)
-        
-        self.treeview['show'] = 'headings'
+        self.edit_opcoes_btn = ttk.Button(self.header,
+                                          text='Editar',
+                                          bootstyle='SUCCESS-OUTLINE',
+                                          command=self.abrir_janela_especial
+                                          )
+        self.edit_opcoes_btn.pack(pady=10, padx=10, side='left')
+
+
+    def event_RadioButton(self):  # Chamado quando algum radio button é pressionado
+        print("Radio btn pressionado")
+
+        if self.option_selected.get() == "Estados":
+            self.cb_input_municipio.configure(state="disabled")
+            self.cb_input_uf.configure(state="normal")
+
+            # self.cb_input_uf['values'] = self.todos_uf # Isso só precisa ser feito uma vez, quando a planilha é carregada
+
+        elif self.option_selected.get() == "Municípios":
+            self.cb_input_uf.configure(state="normal")
+            self.cb_input_municipio.configure(state="normal")
+
+            # self.cb_input_uf['values'] = self.todos_uf # Isso só precisa ser feito uma vez, quando a planilha é carregada
+            self.cb_input_municipio['values'] = dados_tabela.get_uf_municipios(
+                tabela=self.tabela,
+                estado=self.cb_input_uf.get()
+            )
+
+    def TextBoxUpdate(self, event):  # Chamado quando algum estado é selecionado
+        self.cb_input_municipio.set("Selecionar Município")
+        self.cb_input_municipio['values'] = dados_tabela.get_uf_municipios(
+            tabela=self.tabela,
+            estado=self.cb_input_uf.get()
+        )
+
+    def processar(self):
+        if self.option_selected.get() == "Estados":
+            resultados = dados_tabela.get_uf(self.tabela, self.cb_input_uf.get(
+            ), self.contas_selecionadas, dados_tabela.coluna_interesse)
+        elif self.option_selected.get() == "Municípios":
+            resultados = dados_tabela.get_cidade(self.tabela, self.cb_input_municipio.get(
+            ), self.contas_selecionadas, dados_tabela.coluna_interesse)
+
         self.reset_treeview()
 
-        for column in self.treeview['columns']:
-            self.treeview.column(column, width=180)
-            self.treeview.heading(column, text=column)
-
-        for index, row in enumerate(resultados.index.values):
+        for index, row in sorted(enumerate(resultados.index.values), key=lambda ir: ir[1]):
             lista_valores = list(resultados.iloc[index])
 
             for index_lista in range(len(lista_valores)):
@@ -93,14 +169,62 @@ class Interfazinha:
 
         self.treeview.pack(pady=50, padx=100)
         return self.treeview
-    
+
     def reset_treeview(self):
         self.treeview.delete(*self.treeview.get_children())
+
+    def carregar_planilha(self):
+        self.carregar_btn['text'] = "Carregando..."
+        self.carregar_btn.config(state='disabled')
+
+        try:
+            filename = filedialog.askopenfilename(
+                initialdir=pathlib.Path(),
+                title="Select a File",
+                filetypes=(
+                    ("Supported files", ["*.csv*", "*.xlsx*", "*.xls*", "*.xlsx*",
+                                         "*.xlsm*", "*.xlsb*", "*.odf*", "*.ods*", "*.odt*"]),
+                    ("all files", "*.*")
+                )
+            )
+
+            if (len(filename) == 0):
+                raise Exception("User cancelou")
+
+            path_file = pathlib.Path(filename)
+
+            if path_file.suffix == ".csv":
+                df = pd.read_csv(path_file)
+            else:  # Excel
+                df = pd.read_excel(
+                    path_file, skiprows=4)
+
+            self.carregar_tabela(df)
+
+            messagebox.showinfo("Planilha Carregada",
+                                "Planilha Carregada com Sucesso!")
+        except KeyError as e:
+            messagebox.showerror(
+                "Error ao carregar planilha", "Ocorreu algum problema ao tentar carregar a planilha!\n" +
+                f"\nColuna ('{e.args[0]}') não foi encontrada.\n" +
+                "\nVerifique se você informou corretamente a linha das colunas.")
+        except Exception as e:
+            messagebox.showerror(
+                "Error ao carregar planilha", "Ocorreu algum problema ao tentar carregar a planilha!")
+
+        self.carregar_btn['text'] = "Carregar planilha"
+        self.carregar_btn.config(state='normal')
 
 
 if __name__ == "__main__":
     # pegar tabela do arquivo dados_tabela
-    tabela = dados_tabela.get_tabela()
 
     interface = Interfazinha()
-    interface.app.mainloop() 
+    interface.criar_janela()
+    
+    try:
+        interface.carregar_tabela(dados_tabela.get_tabela())
+    except:
+        print("Deu erro ao carregar a tabela, isso deve acontecer no pc do cara, deboa.")
+    
+    interface.app.mainloop()
--- projeto_cont.py
+++ projeto_cont.py
@@ -26,17 +26,25 @@ def get_tabela():
 
     return df_valor_seguridade_social"""
 
-def get_all_municipios(tabela: pd.DataFrame) -> list:
+def get_all_municipios(tabela: pd.DataFrame) -> list[str]:
     df_municipio = tabela['Instituição'].unique()
     list_municipio = list(df_municipio)
     list_municipio.sort()
     return list_municipio
 
-def get_all_uf(tabela: pd.DataFrame) -> list:
+def get_all_uf(tabela: pd.DataFrame) -> list[str]:
     df_estados = tabela['UF'].unique()
     list_estados = list(df_estados)
     list_estados.sort()
     return list_estados
+
+def get_uf_municipios(tabela: pd.DataFrame, estado : str) -> list[str]:
+    tabela_estado = tabela.loc[tabela['UF'] == estado]
+    df_muni_uf = tabela_estado['Instituição'].unique()
+    lista_munici_uf = list(df_muni_uf)
+    lista_munici_uf.sort()
+    return lista_munici_uf
+    
 
 def get_cidade(tabela: pd.DataFrame, cidade: str, contas_interesse, coluna_interesse) -> pd.DataFrame:
     df_cidade = tabela.loc[tabela['Instituição'] == cidade]
@@ -48,10 +56,10 @@ def get_cidade(tabela: pd.DataFrame, cidade: str, contas_interesse, coluna_inter
         for conta in contas_interesse:
             dicio[coluna][conta] = df_cidade.loc[
                 (df_cidade['Coluna'] == coluna) & 
-                (df_cidade['Conta'] == conta)][['Valor']].values
-            value = dicio[coluna][conta]
-            if value.size >= 1:
-                dicio[coluna][conta] = value.max()
+                (df_cidade['Conta'] == conta)]['Valor'].sum()
+            # value = dicio[coluna][conta]
+            # if value.size >= 1:
+            #     dicio[coluna][conta] = value.max()
     
     tabela_cidade = pd.DataFrame.from_dict(dicio)
     pd.set_option('float_format', '{:.2f}'.format)
@@ -68,34 +76,41 @@ def get_uf(tabela: pd.DataFrame, estado: str, contas_interesse, coluna_interesse
         for conta in contas_interesse:        
             array_soma_acumulativa = df_estado.loc[
                 (df_estado['Coluna'] == coluna) &
-                (df_estado['Conta'] == conta)]['Valor'].cumsum()
+                (df_estado['Conta'] == conta)]['Valor'].sum()
 
-            dicio[coluna][conta] = array_soma_acumulativa.values.max()
+            dicio[coluna][conta] = array_soma_acumulativa
             
     tabela_uf = pd.DataFrame.from_dict(dicio)
     pd.set_option('float_format', '{:,.2f}'.format)
     dicio.clear()
     return tabela_uf
 
-coluna_interesse = ['Despesas Empenhadas', 'Despesas Liquidadas', 'Despesas Pagas']
+coluna_interesse = [
+    'Despesas Empenhadas',
+     'Despesas Liquidadas',
+      'Despesas Pagas',
+      'Inscrição de Restos a Pagar Não Processados',
+    'Inscrição de Restos a Pagar Processados',
+      ]
 
 contas_interesse = [
     '06 - Segurança Pública', 
     '08 - Assistência Social', 
     '09 - Previdência Social', 
     '10 - Saúde', 
+    '12 - Educação',
     '10.301 - Atenção Básica',
     '10.302 - Assistência Hospitalar e Ambulatorial', 
     '10.303 - Suporte Profilático e Terapêutico', 
     '10.304 - Vigilância Sanitária', 
     '10.305 - Vigilância Epidemiológica', 
     '10.306 - Alimentação e Nutrição',
-    '12 - Educação',
     ]
 
 
 if __name__ == "__main__":
     tabela = get_tabela()
+    get_uf_municipios(tabela_com_tudo=tabela, estado='PE')
     #valor_seguridade = get_seguridade_social(tabela, 'Prefeitura Municipal de Recife - PE')
    #print(valor_seguridade)
     #print(tabela)
